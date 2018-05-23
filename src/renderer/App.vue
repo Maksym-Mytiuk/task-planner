7bc7de5a621ba21b18661695fefc84d000dbc224
Maksym Mytiuk <Maksym.Mytiuk@infopulse.com>
2018-05-23 17:36:19 +0300

    add(task-runner): starter pack

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'task-planner',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
